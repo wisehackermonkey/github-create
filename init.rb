844a08d4d79fb868973bf637eb3cdd02c1b0b90c
wisehackermonkey <oranbusiness@gmail.com>
2019-02-06 08:48:01 +0000

    cleaned up output formating, handled pre-existing repo, added comments

--- init.rb
+++ init.rb
@@ -1,6 +1,20 @@
 #!/usr/bin/env ruby
+# Github repository creator
+
+# Creates a new repository on github.com
+# asks for project name => repository name
+# asks for username and password of github account
+# generates a repository based on the name and description
+
+# wisemonkey
+# oranbusiness@gmail.com
+# 20190206
+# github.com/wisehackermonkey
+
+
 require 'github_api'
 
+# helper module for no echoing password input
 require_relative 'lib/helper.rb'
 include Helper
 
@@ -19,7 +33,7 @@ username = gets.chomp
 
 print "Github Password: "
 password = Helper::get_password
-
+puts
 
 
 begin
@@ -42,6 +56,7 @@ begin
     # puts "https://www.github.com/#{username}/#{project_name}"
     
 rescue Github::Error::GithubError => e
+
     # handle if repo was not found
     if e.is_a? Github::Error::NotFound
         puts "-" * 44
@@ -49,9 +64,17 @@ rescue Github::Error::GithubError => e
         puts "-" * 44
     end
     
+    # Handles incorrect username or password login
     if e.is_a? Github::Error::Unauthorized
         puts "-" * 44
         puts "Error: username or password is incorrect"
+        puts "-" * 44
+    end
+    
+    # Handels if repository already exists
+    if e.is_a? Github::Error::UnprocessableEntity
+        puts "-" * 44
+        puts "Error: Repository already exists, try another project name"
         puts "-" * 44
     end
     
--- lib/helper.rb
+++ lib/helper.rb
@@ -1,7 +1,14 @@
+# Helper functions for init.rb
+
+# wisemonkey
+# oranbusiness@gmail.com
+# 20190206
+# github.com/wisehackermonkey
+
 module Helper
     require 'io/console'
     # Hides users password to when typed into shell
-    # Source
+    # Source https://stackoverflow.com/questions/2338889/how-to-hide-password-input-from-terminal-in-ruby-script
     def get_password
         password = STDIN.noecho(&:gets).chomp
     end
